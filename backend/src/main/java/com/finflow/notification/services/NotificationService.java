a30156241599b511ea28c2c35221ab092aeefa73
Suman <sumankarkii897@gmail.com>
2026-08-15 15:46:32 +0545

    test: add notification tests

--- backend/src/main/java/com/finflow/notification/services/NotificationService.java
+++ backend/src/main/java/com/finflow/notification/services/NotificationService.java
@@ -2,7 +2,7 @@ package com.finflow.notification.services;
 
 import com.finflow.auth_users.entity.User;
 import com.finflow.notification.dtos.request.NotificationRequest;
-import com.finflow.notification.dtos.response.NotificationResponse;
+
 
 public interface NotificationService {
 void sendEmail(NotificationRequest notificationRequest, User user);
